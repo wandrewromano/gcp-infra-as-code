c715bcaab04458aa309ef860274013e3392e771f
Andrew Romano <andrewr@ecdev.ensenta.net>
2026-08-14 09:05:13 -0500

    task 03

--- 003_variables_and_outputs/task/outputs.tf
+++ 003_variables_and_outputs/task/outputs.tf
@@ -1,10 +1,9 @@
-# TODO: output "bucket_url" {
-#   value = google_storage_bucket.my_bucket.___
-#   # Check the Attributes Reference on the google_storage_bucket
-#   # docs page (linked in 002_create_storage_bucket) for the
-#   # attribute that holds the gs:// URL.
-# }
+output "bucket_url" {
+  description = "bucket URL"
+  value = google_storage_bucket.my_bucket.url
+}
 
-# TODO: output "bucket_self_link" {
-#   value = google_storage_bucket.my_bucket.___
-# }
+output "bucket_self_link" {
+  description = "bucket self link"
+  value = google_storage_bucket.my_bucket.self_link
+}
--- 003_variables_and_outputs/task/variables.tf
+++ 003_variables_and_outputs/task/variables.tf
@@ -1,15 +1,12 @@
-# TODO: variable "project_id" {
-#   type        = string
-#   description = "GCP project ID to deploy resources into."
-#   # no default — Terraform will require it (via terraform.tfvars or -var)
-# }
+variable "project_id" {
+    type = string
+}
 
-# TODO: variable "region" {
-#   type    = string
-#   default = "us-central1"
-# }
+variable "region" {
+    type = string
+    default = "us-central1"
+}
 
-# TODO: variable "bucket_name" {
-#   type = string
-#   # no default — must be supplied, same reasoning as project_id
-# }
+variable "bucket_name" {
+    type = string
+}
