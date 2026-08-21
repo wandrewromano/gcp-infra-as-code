426ecfee432075722329ba72d4cd326133e38e2a
Andrew Romano <andrewr@ecdev.ensenta.net>
2026-08-21 07:33:51 -0500

    006

--- 006_upload_bucket_object/task/main.tf
+++ 006_upload_bucket_object/task/main.tf
@@ -19,6 +19,7 @@ resource google_storage_bucket "my_bucket" {
   name = var.bucket_name
   location = var.region
   uniform_bucket_level_access = true
+  force_destroy = true
 
   versioning {
     enabled = true
