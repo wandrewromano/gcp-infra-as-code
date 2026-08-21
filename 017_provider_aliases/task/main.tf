87df9da4c6fde73c372eb858438175de066a0ce6
Andrew Romano <andrewr@ecdev.ensenta.net>
2026-08-21 16:56:07 -0500

    017

--- 017_provider_aliases/task/main.tf
+++ 017_provider_aliases/task/main.tf
@@ -14,6 +14,32 @@ provider "google" {
   region  = var.region
 }
 
+provider "google" {
+  alias = "europe"
+  project = var.project_id
+  region = "europe-west1"
+}
+
+resource "google_compute_network" "this" {
+    name                    = "my-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "us" {
+  name          = "my-subnet-us"
+  ip_cidr_range = "192.168.0.0/24"  # a /24, e.g. "10.0.1.0/24"
+  network       = google_compute_network.this.id  # reference the network resource above by its `id`
+}
+
+resource "google_compute_subnetwork" "europe" {
+  provider = google.europe
+  name          = "my-subnet-europe"
+  ip_cidr_range = "192.168.0.0/24"  # a /24, e.g. "10.0.1.0/24"
+  network       = google_compute_network.this.id  # reference the network resource above by its `id`
+}
+
+
+
 # TODO: provider "google" { alias = "europe", project = var.project_id, region = "europe-west1" }
 
 # TODO: google_compute_network "this" (global, default provider)
