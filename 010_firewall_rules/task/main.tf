074e40e9041513a6f0cff74dac2fbab86f57fe8d
Andrew Romano <andrewr@ecdev.ensenta.net>
2026-08-21 09:39:16 -0500

    010

--- 010_firewall_rules/task/main.tf
+++ 010_firewall_rules/task/main.tf
@@ -15,32 +15,43 @@ provider "google" {
 }
 
 # TODO: google_compute_network + google_compute_subnetwork (from exercise 008)
+resource "google_compute_network" "my_network" {
+  name                    = "my-network"
+  auto_create_subnetworks = false
+}
 
-# TODO: resource "google_compute_firewall" "allow_ssh_iap" {
-#   name    = "___"
-#   network = ___  # reference the network resource above by its `id`
-#
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["22"]
-#   }
-#
-#   source_ranges = ["35.235.240.0/20"]
-#   target_tags   = ["ssh"]
-# }
+resource "google_compute_subnetwork" "example" {
+  name          = "my-subnet"
+  ip_cidr_range = "192.168.0.0/24"  # a /24, e.g. "10.0.1.0/24"
+  region        = var.region
+  network       = google_compute_network.my_network.id  # reference the network resource above by its `id`
+}
 
-# TODO: resource "google_compute_firewall" "allow_http" {
-#   name    = "___"
-#   network = ___
-#
-#   allow {
-#     protocol = "tcp"
-#     ports    = ["80"]
-#   }
-#
-#   source_ranges = ["0.0.0.0/0"]
-#   target_tags   = ["http-server"]
-# }
+resource "google_compute_firewall" "allow_ssh_iap" {
+  name    = "allow-iap-22"
+  network = google_compute_network.my_network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["35.235.240.0/20"]
+  target_tags   = ["ssh"]
+}
+
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http-80"
+  network = google_compute_network.my_network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
 #
 # Optional: the google_compute_firewall docs page above has more under
 # Argument Reference, e.g. `priority`, `direction`, `deny`.
