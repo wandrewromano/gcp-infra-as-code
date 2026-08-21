3be57eb597543ff0e2488db41ecfa202fd5bfc23
Andrew Romano <andrewr@ecdev.ensenta.net>
2026-08-21 10:19:35 -0500

    012

--- 012_dynamic_blocks/task/main.tf
+++ 012_dynamic_blocks/task/main.tf
@@ -14,7 +14,40 @@ provider "google" {
   region  = var.region
 }
 
-# TODO: google_compute_network + google_compute_subnetwork (from 008/010)
+## Implemented via `gcloud services enable compute.googleapis.com --project training-project-11-f0a7d1`
+# resource "google_project_service" "compute" {
+#   project            = var.project_id
+#   service            = "compute.googleapis.com"
+#   disable_on_destroy = false
+# }
+
+# TODO: google_compute_network + google_compute_subnetwork (from exercise 008)
+resource "google_compute_network" "my_network" {
+  name                    = "my-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "example" {
+  name          = "my-subnet"
+  ip_cidr_range = "192.168.0.0/24" # a /24, e.g. "10.0.1.0/24"
+  region        = var.region
+  network       = google_compute_network.my_network.id # reference the network resource above by its `id`
+}
+
+resource "google_compute_firewall" "allow_rules" {
+  name    = "allow-rules"
+  network = google_compute_network.my_network.id
+
+  dynamic "allow" {
+    for_each = var.allowed_ports
+    content {
+      protocol = allow.value.protocol
+      ports = allow.value.ports
+    }
+  }
+
+  source_ranges = ["35.235.240.0/20"]
+}
 
 # TODO: google_compute_firewall "this" with:
 # - dynamic "allow" { for_each = var.allowed_ports; content { ... } }
--- 012_dynamic_blocks/task/variables.tf
+++ 012_dynamic_blocks/task/variables.tf
@@ -10,3 +10,14 @@ variable "region" {
 }
 
 # TODO: variable "allowed_ports" (list of objects with protocol/ports)
+variable "allowed_ports" {
+  type = list(object({
+    protocol = string
+    ports    = list(string)
+  }))
+  default = [
+    { protocol = "tcp", ports = ["22"] },
+    { protocol = "tcp", ports = ["80", "443"] },
+    { protocol = "tcp", ports = ["3389"] }
+  ]
+}
