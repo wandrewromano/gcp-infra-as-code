ae3694cf9602b896daa80b898317ed97d9dbe6a9
Andrew Romano <andrewr@ecdev.ensenta.net>
2026-08-21 07:34:25 -0500

    009

--- 009_builtin_functions/task/main.tf
+++ 009_builtin_functions/task/main.tf
@@ -14,13 +14,37 @@ provider "google" {
   region  = var.region
 }
 
-# TODO: google_compute_network "this" (custom mode)
+locals {
+  name_prefix   = "${var.project_id}-${var.region}"
+}
 
-# TODO: google_compute_subnetwork "this" with for_each = toset(var.subnet_names)
-# - ip_cidr_range = cidrsubnet(var.network_cidr, 8, index(var.subnet_names, each.value))
-# - name          = format("%s-%s-subnet", var.project_id, each.value)
+resource "google_compute_network" "this" {
+  name                    = "my-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "this" {
+  for_each = toset(var.subnet_names)
+  name          = format("%s-%s-subnet", var.project_id, each.value)
+  ip_cidr_range = cidrsubnet(var.network_cidr, 8, index(var.subnet_names, each.value))
+  region        = var.region
+  network       = google_compute_network.this.id 
+}
+
 
 # TODO: google_storage_bucket "this" (reuse the pattern from earlier exercises)
+resource "google_storage_bucket" "this"{
+  name = "${local.name_prefix}-bucket-009"
+  location = var.region
+  uniform_bucket_level_access = true
+  force_destroy = true
+}
 
-# TODO: google_storage_bucket_object "network_manifest" with
-# content = jsonencode({ network_cidr = var.network_cidr, subnets = { ... } })
+resource "google_storage_bucket_object" "network_manifest" {
+  name    = "network-manifest.json"
+  bucket  = google_storage_bucket.this.name
+  content = jsonencode({
+    network_cidr = var.network_cidr
+    subnets      = { for name, s in google_compute_subnetwork.this : name => s.ip_cidr_range }
+  })
+}
--- 009_builtin_functions/task/terraform.tfvars
+++ 009_builtin_functions/task/terraform.tfvars
@@ -1,4 +1,4 @@
-project_id   = "your-gcp-project-id"
+project_id   = "training-project-11-f0a7d1"
 region       = "us-central1"
 network_cidr = "10.0.0.0/16"
 subnet_names = ["dev", "staging", "prod"]
